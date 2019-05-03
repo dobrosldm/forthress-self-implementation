e07e75ed8c6fe3c1fcf919e572b1001e0cf9a341
Dimon <official.dobrosldm@gmail.com>
2019-05-03 19:02:02 +0300

    adding macroses for return stack manipulations

--- src/forthress.asm
+++ src/forthress.asm
@@ -4,8 +4,11 @@ global _start
 
 %include "macro.inc"
 
+; command counter
 %define pc r15
+; address for current instruction
 %define w r14
+; return stack position
 %define rstack r13
 
 ;-------------------------------------------------------------
@@ -18,31 +21,38 @@ section .text
 section .bss
 
 ; global data for user
-user_mem: resq 65536
+user_mem:
+	resq 65536
 ; returns stack end
 resq 1023              
 ; returns stack start
-rstack_start: resq 1 
+rstack_start:
+	resq 1 
 
 ;-------------------------------------------------------------
 section .data
 
 ; a pointer to the last word in the dictionary
-last_word: dq _lw      
+last_word:
+	dq _lw      
 ; pointer to current global data
-dp: dq user_mem
+dp:
+	dq user_mem
 ; address of data stack
-stack_start:  dq 0 
+stack_start:
+	dq 0 
 
 ;-------------------------------------------------------------
 section .text
 
+; virtual machine starting point
 _start: 
     mov rstack, rstack_start
     mov [stack_start], rsp
     ;  setting up initial stack state
     mov pc, forth_init
 
+; Forthress inner-interpreter
 next:                  
     mov w, pc
     add pc, 8
